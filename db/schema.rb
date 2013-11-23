76d2cc8a05165ab15443fba0b9ef33c2fcb99cdb
Will Holcomb <will@dhappy.org>
2013-11-23 08:40:24 -0500

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131123040443) do
+ActiveRecord::Schema.define(version: 20131123133959) do
 
   create_table "shifts", force: true do |t|
     t.datetime "start"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20131123040443) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
