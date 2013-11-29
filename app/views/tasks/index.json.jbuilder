bfe435f45174a854aa2d7cea4cf76ac52e46030c
Will Holcomb <will@dhappy.org>
2013-11-29 01:42:47 -0500

    adding icon to task

--- app/views/tasks/index.json.jbuilder
+++ app/views/tasks/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@tasks) do |task|
-  json.extract! task, :id, :name, :description
+  json.extract! task, :id, :name, :description, :icon
   json.url task_url(task, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131124164637) do
+ActiveRecord::Schema.define(version: 20131129042640) do
 
   create_table "shifts", force: true do |t|
     t.datetime "start"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20131124164637) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "icon"
   end
 
   create_table "users", force: true do |t|
