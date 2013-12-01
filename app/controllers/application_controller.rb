0cdb305d52f40b8b607772464a223ddfcc8cd298
Will Holcomb <will@dhappy.org>
2013-12-01 14:53:26 -0500

    trying to fix forbidden attributes error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # https://github.com/ryanb/cancan/issues/835#issuecomment-18663815
+  before_filter do
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:alert] = "Access denied."
     redirect_to new_user_registration_url
