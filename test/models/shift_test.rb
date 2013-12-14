3e4573c823066e7a3421dfbefcb9201c64a712e4
Will Holcomb <will@dhappy.org>
2013-12-14 15:28:29 -0500

    enabling rake test test/models/shift_test.rb

--- test/models/shift_test.rb
+++ test/models/shift_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
+include ShiftsHelper
 
 class ShiftTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "days size" do
+    start_date = Time.new
+    end_date = start_date + 1.week
+    assert_equal 7, days(start_date, end_date).count
+  end
 end
