fcce028168c5d32d2a4e93ed4a974744d6de2c52
Will Holcomb <will@dhappy.org>
2013-12-14 16:09:26 -0500

    testing

--- test/models/shift_test.rb
+++ test/models/shift_test.rb
@@ -5,6 +5,11 @@ class ShiftTest < ActiveSupport::TestCase
   test "days size" do
     start_date = Time.new
     end_date = start_date + 1.week
-    assert_equal 7, days(start_date, end_date).count
+    assert_equal 8, days(start_date, end_date).count
+
+    date_format = '%Y/%m/%d'
+    start_date = DateTime.strptime('2013/12/16', date_format)
+    end_date = DateTime.strptime('2013/12/20', date_format)
+    assert_equal 5, days(start_date, end_date).count
   end
 end
