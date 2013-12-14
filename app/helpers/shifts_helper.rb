7e9938d2ec5d2d4a4cb682b02359c204c595bd79
Will Holcomb <will@dhappy.org>
2013-12-14 13:13:15 -0500

    breaking days iterator out into helper

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,2 +1,9 @@
 module ShiftsHelper
+  def days(start_date, end_date)
+    days = []
+    (start_date.to_i .. end_date.to_i).step(24*60*60) do |timestamp|
+      days << Time.at(timestamp)
+    end
+    days
+  end
 end
