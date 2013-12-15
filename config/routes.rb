1a76ff62d80a63be915a46b618f72fa2d9434fdb
Will Holcomb <will@dhappy.org>
2013-12-15 01:35:49 -0500

    rails g scaffold_controller user

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Volunteers::Application.routes.draw do
 
   resources :tasks
 
+  scope "/admin" do
+    resources :users
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
