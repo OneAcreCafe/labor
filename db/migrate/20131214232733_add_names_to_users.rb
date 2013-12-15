62fdb494fc31cdb06627e23d736b83f671e9bcbe
Will Holcomb <will@dhappy.org>
2013-12-14 21:54:33 -0500

    fixed typo

--- db/migrate/20131214232733_add_names_to_users.rb
+++ db/migrate/20131214232733_add_names_to_users.rb
@@ -2,6 +2,6 @@ class AddNamesToUsers < ActiveRecord::Migration
   def change
     add_column :users, :given_name, :string
     add_column :users, :family_name, :string
-    add_column :users, :nickname, :srting
+    add_column :users, :nickname, :string
   end
 end
