3c525700958177b5555c73152a3c513078d32ca3
Will Holcomb <will@dhappy.org>
2013-12-14 19:09:18 -0500

    enabled editing of user names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   # https://github.com/ryanb/cancan/issues/835#issuecomment-18663815
   before_filter do
     resource = controller_name.singularize.to_sym
@@ -26,5 +28,14 @@ class ApplicationController < ActionController::Base
     end
 
     request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) {
+      |u| u.permit(:given_name, :family_name, :nickname, :email, :password, :password_confirmation, :current_password)
+    }
   end
 end
